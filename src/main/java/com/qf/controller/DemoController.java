5f149b77adc2c94e2ff3dceed762145b4380636a
tangjinsong <tangjinsong@aliyun.com>
2019-11-04 17:42:14 +0800

    唐劲松测试提交

---
+++ src/main/java/com/qf/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.qf.controller;
+
+/**
+ * @author tangjinsong
+ * @Classname DemoController
+ * @Description TODO
+ * @Date 2019/11/4 17:40
+ * @Created by tangjinsong
+ */
+
+public class DemoController {
+    public void method1(){
+
+
+        String  demo1="这是第一次测试";
+    }
+}
